a66e103cd5a3cce2385df9015b5c65dcd08b9a2b
rebeccazzha <110057062+rebeccazzha@users.noreply.github.com>
2023-10-10 23:03:05 -0700

    added inserting recipe

---
+++ static/js/insertDrink.js
@@ -0,0 +1,30 @@
+document.querySelector('#drink-form').addEventListener('submit', async function(e) {
+  e.preventDefault(); // Prevents the default form submission behavior
+
+  const name = document.querySelector('#name').value;
+  const image_url = document.querySelector('#image_url').value;
+  const ingredients = document.querySelector('#ingredients').value;
+  const base = document.querySelector('#base').value;
+  const recipe = document.querySelector('#recipe').value;
+
+  const formData = { name, image_url, ingredients, base, recipe };
+
+  try {
+    const response = await fetch('/api/postDrink', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if(response.ok) {
+      alert('Form submitted successfully!');
+      window.location.href = '../recipe.html';
+    } else {
+      alert('Failed to submit the form. Please try again.');
+    }
+
+  } catch (error) {
+    console.error('Error submitting form:', error);
+  }
+});
